416e818ee95647cf7b771ce560569e5958b59309
artem <lvrbl7654321@gmail.com>
2025-05-13 00:38:43 +0200

    123

--- app/src/main/java/com/example/userprofile/MainActivity.java
+++ app/src/main/java/com/example/userprofile/MainActivity.java
@@ -1,14 +1,20 @@
 package com.example.userprofile;
 
+import android.content.DialogInterface;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.text.InputType;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -19,18 +25,13 @@ public class MainActivity extends AppCompatActivity {
     private Button buttonSubmit;
     private TextView emailTextView;
     private TextView passwordTextView;
-    private Button buttonSubmitEmailDialog;
-    private Button buttonTerminateEmailDialog;
-    private Button buttonSubmitPasswordDialog;
-    private Button buttonTerminatePasswordDialog;
-    private EditText editTextEmailDialog;
-    private EditText editTextPasswordDialogFirst;
-    private EditText editTextPasswordDialogLast;
-    private String email;
-    private String password;
+    private boolean emailChanged = false;
+    private boolean passwordChanged = false;
     private String tempEmail = "";
     private String tempPasswordFirst = "";
     private String tempPasswordLast = "";
+    private int errorType = 0;
+    private int errors = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,60 +54,141 @@ public class MainActivity extends AppCompatActivity {
         buttonSubmit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                customDialogChangeEmail();
+                alertDialogChangeEmail();
             }
         });
     }
-    private void customDialogChangeEmail(){
-        final android.app.Dialog dialog = new android.app.Dialog(this);
-        dialog.setContentView(R.layout.custom_dialog_change_email);
+    private void alertDialogChangeEmail(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Zmień Email");
+        builder.setMessage("Podaj nowy email:");
 
-        buttonSubmitEmailDialog = dialog.findViewById(R.id.buttonSubmitEmailDialog);
-        buttonTerminateEmailDialog = dialog.findViewById(R.id.buttonTerminateEmailDialog);
-        editTextEmailDialog = dialog.findViewById(R.id.editTextEmailDialog);
+        EditText changeEmail = new EditText(this);
+        changeEmail.setHint("Nowy email");
+        builder.setView(changeEmail);
 
-        buttonSubmitEmailDialog.setOnClickListener(new View.OnClickListener() {
+        builder.setPositiveButton("Zapisz", new DialogInterface.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                customDialogChangePassword();
-                dialog.dismiss();
-                tempEmail = String.valueOf(editTextEmailDialog.getText());
+            public void onClick(DialogInterface dialogInterface, int i) {
+                tempEmail = String.valueOf(changeEmail.getText());
+                emailChanged = true;
+                alertDialogChangePassword();
             }
         });
 
-        buttonTerminateEmailDialog.setOnClickListener(new View.OnClickListener() {
+        builder.setNegativeButton("Anuluj", new DialogInterface.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                customDialogChangePassword();
-                dialog.dismiss();
+            public void onClick(DialogInterface dialogInterface, int i) {
+                alertDialogChangePassword();
             }
         });
-        dialog.show();
+        builder.create().show();
     }
-    private void customDialogChangePassword(){
-        final android.app.Dialog dialog = new android.app.Dialog(this);
-        dialog.setContentView(R.layout.custom_dialog_change_password);
 
-        buttonSubmitPasswordDialog = dialog.findViewById(R.id.buttonSubmitPasswordDialog);
-        buttonTerminatePasswordDialog = dialog.findViewById(R.id.buttonTerminatePasswordDialog);
-        editTextPasswordDialogFirst = dialog.findViewById(R.id.editTextPasswordDialogFirst);
-        editTextPasswordDialogLast = dialog.findViewById(R.id.editTextPasswordDialogLast);
+    private void alertDialogChangePassword(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Zmień Hasło");
+        builder.setMessage("Podaj nowe hasło:");
 
-        buttonSubmitPasswordDialog.setOnClickListener(new View.OnClickListener() {
+        LinearLayout layout = new LinearLayout(this);
+        layout.setOrientation(LinearLayout.VERTICAL);
+        layout.setPadding(50, 20, 50, 10); // чуть-чуть отступов
+
+        EditText changePasswordFirst = new EditText(this);
+        changePasswordFirst.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+        layout.addView(changePasswordFirst);
+
+        TextView middleMessage = new TextView(this);
+        middleMessage.setText("Powtórz hasło:");
+        middleMessage.setPadding(0, 20, 0, 10);
+        layout.addView(middleMessage);
+
+        EditText changePasswordLast = new EditText(this);
+        changePasswordLast.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+        layout.addView(changePasswordLast);
+
+        builder.setView(layout);
+
+        builder.setPositiveButton("Zapisz", new DialogInterface.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                tempPasswordFirst = String.valueOf(editTextPasswordDialogFirst.getText());
-                tempPasswordLast = String.valueOf(editTextPasswordDialogLast.getText());
-                dialog.dismiss();
+            public void onClick(DialogInterface dialogInterface, int i) {
+                tempPasswordFirst = String.valueOf(changePasswordFirst.getText());
+                tempPasswordLast = String.valueOf(changePasswordLast.getText());
+                passwordChanged = true;
+                validation();
             }
         });
 
-        buttonTerminatePasswordDialog.setOnClickListener(new View.OnClickListener() {
+        builder.setNegativeButton("Anuluj", new DialogInterface.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                dialog.dismiss();
+            public void onClick(DialogInterface dialogInterface, int i) {
+                validation();
             }
         });
-        dialog.show();
+        builder.create().show();
+    }
+
+    private void validation(){
+        if(emailChanged){
+            if(!(tempEmail.contains("@"))){
+                errorType = 1;
+                errors += 1;
+            }
+        }
+
+        if(passwordChanged){
+            if(!(tempPasswordFirst.equals(tempPasswordLast))){
+                errorType = 2;
+                errors += 1;
+            }
+        }
+
+        if(errors==2){
+            infoMessage.setTextColor(Color.RED);
+            infoMessage.setText("Błąd: Nieprawidłowy format emaila." + "\n" + "Błąd: Hasła nie pasują do siebie.");
+        }
+
+        if(errors==1){
+            if(errorType==1){
+                infoMessage.setTextColor(Color.RED);
+                infoMessage.setText("Błąd: Nieprawidłowy format emaila.");
+            }
+
+            if(errorType==2){
+                infoMessage.setTextColor(Color.RED);
+                infoMessage.setText("Błąd: Hasła nie pasują do siebie.");
+            }
+        }
+
+        if(errors==0){
+            Log.d("123", String.valueOf(emailChanged));
+            Log.d("123", String.valueOf(passwordChanged));
+            if((!emailChanged) && (!passwordChanged)){
+                infoMessage.setTextColor(Color.GRAY);
+                infoMessage.setText("Edycja profilu anulowana");
+            }
+
+            if(errorType==0 && errors==0){
+                if(emailChanged || passwordChanged){
+                    infoMessage.setTextColor(Color.GREEN);
+                    infoMessage.setText("Profil zaktualizowany!");
+                }
+                if(emailChanged){
+                    emailTextView.setText(tempEmail);
+                    tempEmail="";
+                    emailChanged=false;
+                }
+                if(passwordChanged){
+                    passwordTextView.setText(tempPasswordFirst);
+                    tempPasswordFirst="";
+                    tempPasswordLast="";
+                    passwordChanged=false;
+                }
+            }
+        }
+        emailChanged=false;
+        passwordChanged=false;
+        errors=0;
+        errorType = 0;
     }
 }
